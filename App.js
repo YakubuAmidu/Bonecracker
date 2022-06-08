9109477a21984f6eab32f429b7cfcd3750b48fb8
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-06-08 10:01:02 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
+console.log(StatusBar.currentHeight);
 export default function App() {
   return (
-    <View>
-    
-    </View>
+    <>
+    <SafeAreaView style={styles.container}>
+     <View style={styles.search}>
+        <Text>Search</Text>
+     </View>
+
+     <View style={styles.list}>
+      <Text>List</Text>
+     </View>
+    </SafeAreaView>
+
+    <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-
+ container: {
+   flex: 1,
+   marginTop: StatusBar.currentHeight
+ },
+ search: {
+   backgroundColor: "blue",
+   padding: 16
+ },
+ list: {
+   backgroundColor: "green",
+   padding: 16,
+   flex: 1
+ }
 });
