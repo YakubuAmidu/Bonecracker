ac02d5628770e0bc888096a95735b542d63825b6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-06-09 09:28:27 -0700

    Initial commit

---
+++ src/Infrastructure/Theme/index.js
@@ -0,0 +1,13 @@
+import { Colors } from './Colors';
+import { fontWeights, fontSizes } from './Fonts';
+import { Sizes } from './Sizes';
+import { lineHeights, Space } from './Spacing';
+
+export const Theme = {
+  Colors,
+  fontSizes, 
+  fontWeights,
+  lineHeights,
+  Space,
+  Sizes,
+};
