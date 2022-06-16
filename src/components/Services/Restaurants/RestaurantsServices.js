64cde9846ca2a78b218f1d28b64170a603384053
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-06-16 16:31:21 -0700

    Initial commit

--- src/components/Services/Restaurants/RestaurantsServices.js
+++ src/components/Services/Restaurants/RestaurantsServices.js
@@ -1,4 +1,5 @@
 import { mocks } from './Mock';
+import camelize from 'camelize';
 
 export const RestaurantRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
@@ -10,8 +11,13 @@ export const RestaurantRequest = (location = "37.7749295,-122.4194155") => {
   })
 };
 
-RestaurantRequest().then((result) => {
-  console.log(result);
+const RestaurantTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+}
+
+RestaurantRequest().then(RestaurantTransform).then((transformedResponse) => {
+  console.log(transformedResponse);
 }).catch((err) => {
   console.log(err);
-});
+})
